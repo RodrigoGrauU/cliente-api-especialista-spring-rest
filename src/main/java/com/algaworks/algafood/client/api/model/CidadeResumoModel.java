4b85e62acb7327a0063dc53606f446bc277924fe
Rodrigo Vasconcelos <rodrigoassis1997@gmail.com>
2022-01-16 20:23:44 -0300

    16.12. Desafio: Implementando uma requisição POST no client Java

---
+++ src/main/java/com/algaworks/algafood/client/api/model/CidadeResumoModel.java
@@ -0,0 +1,12 @@
+package com.algaworks.algafood.client.api.model;
+
+import lombok.Data;
+
+@Data
+public class CidadeResumoModel {
+
+  private Long id;
+  private String nome;
+  private String estado;
+
+}
